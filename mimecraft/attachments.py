4a57351c6e03623ac045a862131db12802d1a72e
Lars Kellogg-Stedman <lars@oddbit.com>
2012-08-22 08:06:37 -0400

    moved attachment-handling code into a separate module

---
+++ mimecraft/attachments.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+
+import email.message
+import email.mime.nonmultipart
+import email.mime.multipart
+
+def attach_parts(msg, parts):
+    for p in parts:
+        p_type, p_subtype = p['type'].split('/', 1)
+
+        if 'content' in p:
+            payload = email.mime.nonmultipart.MIMENonMultipart(
+                    p_type,
+                    p_subtype)
+            payload.set_payload(p['content'])
+        elif 'source' in p:
+            name = p.get('name', os.path.basename(p['source']))
+            id = p.get('id', name)
+
+            payload = email.mime.nonmultipart.MIMENonMultipart(
+                    p_type,
+                    p_subtype,
+                    name=name)
+
+            # If we are building a multipart/related part,
+            # assign parts a content-id (for use with the `cid:` url
+            # scheme) and `inline` content-disposition.
+            if msg['content-type'] == 'multipart/related':
+                payload['Content-ID'] = '<%s>' % id
+                payload['Content-Disposition'] = 'inline; filename="%s"' % name
+
+            data = open(p['source']).read()
+
+            # Base64 encode non-text attachments.
+            if not p_type == 'text':
+                data = base64.encodestring(data)
+                payload['encoding'] = 'base64'
+                payload['Content-Transfer-Encoding'] = 'base64'
+            elif p.get('encoding') == 'quoted-printable':
+                data = quopri.encodestring(data)
+                payload['Content-Transfer-Encoding'] = 'quoted-printable'
+
+            payload.set_payload(data)
+        elif 'parts' in p and p_type == 'multipart':
+            payload = email.mime.multipart.MIMEMultipart(
+                    p_subtype)
+            attach_parts(payload, p['parts'])
+
+        msg.attach(payload)
+
+def build_multipart (opts):
+    msg = email.message.Message()
+    msg.set_type(opts.type)
+
+    for hdr_name, hdr_val in opts.header:
+        msg[hdr_name] = hdr_val
+
+    if opts.to_addr:
+        msg['To'] = opts.to_addr
+
+    if opts.from_addr:
+        msg['From'] = opts.from_addr
+
+    if opts.subject:
+        msg['Subject'] = opts.subject
+
+    if opts.cc:
+        msg['Cc'] = opts.cc
+    
+    if opts.parts:
+        attach_parts(msg, opts.parts[0]['parts'])
+
+    return msg
--- mimecraft/main.py
+++ mimecraft/main.py
@@ -3,13 +3,11 @@
 import os
 import sys
 import argparse
-import email.message
-import email.mime.nonmultipart
-import email.mime.multipart
 import email.generator
 import base64
 import quopri
 
+import attachments
 from actions import *
 
 def parse_args():
@@ -65,74 +63,6 @@ def parse_args():
 
     return p.parse_args()
 
-def attach_parts(msg, parts):
-    for p in parts:
-        p_type, p_subtype = p['type'].split('/', 1)
-
-        if 'content' in p:
-            payload = email.mime.nonmultipart.MIMENonMultipart(
-                    p_type,
-                    p_subtype)
-            payload.set_payload(p['content'])
-        elif 'source' in p:
-            name = p.get('name', os.path.basename(p['source']))
-            id = p.get('id', name)
-
-            payload = email.mime.nonmultipart.MIMENonMultipart(
-                    p_type,
-                    p_subtype,
-                    name=name)
-
-            # If we are building a multipart/related part,
-            # assign parts a content-id (for use with the `cid:` url
-            # scheme) and `inline` content-disposition.
-            if msg['content-type'] == 'multipart/related':
-                payload['Content-ID'] = '<%s>' % id
-                payload['Content-Disposition'] = 'inline; filename="%s"' % name
-
-            data = open(p['source']).read()
-
-            # Base64 encode non-text attachments.
-            if not p_type == 'text':
-                data = base64.encodestring(data)
-                payload['encoding'] = 'base64'
-                payload['Content-Transfer-Encoding'] = 'base64'
-            elif p.get('encoding') == 'quoted-printable':
-                data = quopri.encodestring(data)
-                payload['Content-Transfer-Encoding'] = 'quoted-printable'
-
-            payload.set_payload(data)
-        elif 'parts' in p and p_type == 'multipart':
-            payload = email.mime.multipart.MIMEMultipart(
-                    p_subtype)
-            attach_parts(payload, p['parts'])
-
-        msg.attach(payload)
-
-def build_multipart (opts):
-    msg = email.message.Message()
-    msg.set_type(opts.type)
-
-    for hdr_name, hdr_val in opts.header:
-        msg[hdr_name] = hdr_val
-
-    if opts.to_addr:
-        msg['To'] = opts.to_addr
-
-    if opts.from_addr:
-        msg['From'] = opts.from_addr
-
-    if opts.subject:
-        msg['Subject'] = opts.subject
-
-    if opts.cc:
-        msg['Cc'] = opts.cc
-    
-    if opts.parts:
-        attach_parts(msg, opts.parts[0]['parts'])
-
-    return msg
-
 def main():
     import pprint
     opts = parse_args()
@@ -144,7 +74,7 @@ def main():
         pprint.pprint(opts.parts)
         sys.exit()
 
-    msg = build_multipart(opts)
+    msg = attachments.build_multipart(opts)
     g = email.generator.Generator(sys.stdout, mangle_from_=False)
     g.flatten(msg)
     
